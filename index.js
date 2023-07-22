84f2109a2dc569c33cb5ddc4721a860ed466d2b4
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-07-22 22:33:29 +0300

    Created add function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Connected");
+let num1 = null;
+let num2 = null;
+let operator = null;
+let click = false;
+let clickedButton = null; 
+const operators = ["+", "-", "x", "÷"]
+
+const operationDisplay = document.querySelector(".operation");
+const resultDisplay = document.querySelector(".result");
+
+function add(num1, num2) {
+    result = num1 + num2;
+    result = Math.round(result * 1000) / 1000;
+    return result;
+}
